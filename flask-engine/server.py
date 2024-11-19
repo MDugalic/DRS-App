c70b5b45b2674fddf20baad5203d2d57311e2a05
Bogdan <bogdan.drljevic123@gmail.com>
2024-11-19 17:52:53 +0100

    Set up db connection

--- flask-engine/server.py
+++ flask-engine/server.py
@@ -1,6 +1,21 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import text
 
 app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:qwer@localhost/drs_db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+@app.route('/check_connection')
+def check_connection():
+    try:
+        db.session.execute(text('SELECT 1'))
+        return "W"
+    except Exception as e:
+        return f"L {str(e)}"
 
 @app.route("/members")
 def members():
