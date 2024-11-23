ce52334dfdbcd1f81ced0ae51cd6d7c64cb86d6c
B. Drljević <87530056+Drxy0@users.noreply.github.com>
2024-11-23 15:43:36 +0100

    Update run.py, Create User model and requirements.txt

---
+++ flask-engine/app/__init__.py
@@ -0,0 +1,41 @@
+from flask import Flask
+from flask_smorest import Api
+import models
+from flask_migrate import Migrate
+
+import os
+from database import db
+
+#We can provide an optional parameter for the db connection string
+def create_app(db_url=None):
+    app = Flask(__name__)
+
+    init_swagger(app)           #Configure swagger settings
+    configure_db(app, db_url)   #Configure database
+    api = Api(app)              #Connect flask-smorest with the app
+
+    return app
+
+def init_swagger(app):
+    app.config["PROPAGATE_EXCEPTIONS"] = True
+    app.config["API_TITLE"] = "Stores REST API"
+    app.config["API_VERSION"] = "v1"
+    app.config["OPENAPI_VERSION"] = "3.0.3"
+    app.config["OPENAPI_URL_PREFIX"] = "/" 
+    app.config["OPENAPI_SWAGGER_UI_PATH"] = "/swagger-ui"
+    app.config["OPENAPI_SWAGGER_UI_URL"] = "https://cdn.jsdelivr.net/npm/swagger-ui-dist/"
+
+def configure_db(app, db_url=None):
+    #if we pass in db_url it will load that string, 
+    #if we don't pass db_url, it will check env for database_url
+    #if it is not found it will take the 2nd string
+    app.config['SQLALCHEMY_DATABASE_URI'] = db_url or os.getenv("DATABASE_URL", 'mysql://root:qwer@localhost/drs_db')
+    #smth smth good
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    #initialises sqlalchemy extension. We give it our flask app so it can connect it to sqlalchemy
+    db.init_app(app)
+    migrate = Migrate(app, db)  #Alembic migrations
+
+app = create_app()
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ flask-engine/app/models/user.py
@@ -0,0 +1,24 @@
+from database import db
+
+class User(db.Model):
+    __tablename__ = 'users'
+
+    id = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.String(50), nullable=False)
+    last_name = db.Column(db.String(50), nullable=False)
+    address = db.Column(db.String(100))
+    city = db.Column(db.String(50))
+    country = db.Column(db.String(50))
+    phone_number = db.Column(db.String(20))
+
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    username = db.Column(db.String(50), unique=True, nullable=False)
+    password = db.Column(db.String(255), nullable=False)
+
+    # 'user', 'admin'
+    role = db.Column(db.String(20), nullable=False, default="user")
+
+    #same as public override ToString()
+    #__str__ has the same function, but this is preferred for development
+    def __repr__(self):
+        return f"<User: {self.username}>"
