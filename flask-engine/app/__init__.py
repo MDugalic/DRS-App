7f18c79f45b85659aac3bd58ccc974aa36cf3e59
B. Drljević <87530056+Drxy0@users.noreply.github.com>
2024-11-24 23:18:15 +0100

    Begin flask-mail integration

--- flask-engine/app/__init__.py
+++ flask-engine/app/__init__.py
@@ -4,6 +4,7 @@ from flask_migrate import Migrate
 from .routes.users import users_bp
 from flask_cors import CORS
 from flask_jwt_extended import JWTManager
+from .services import mail
 
 import os
 from .database import db
@@ -12,10 +13,11 @@ from .database import db
 def create_app(db_url=None):
     app = Flask(__name__)
 
-    init_swagger(app)           #Configure swagger settings
-    configure_db(app, db_url)   #Configure database
-    api = Api(app)              #Connect flask-smorest with the app
-    migrate = Migrate(app, db)  #Alembic migrations
+    init_swagger(app)           # Configure swagger settings
+    configure_db(app, db_url)   # Configure database
+    configure_mail(app)         # Configure mail
+    api = Api(app)              # Connect flask-smorest with the app
+    migrate = Migrate(app, db)  # Alembic migrations
     CORS(app, origins="http://localhost:3000")
 
     #jwt setup
@@ -43,6 +45,15 @@ def configure_db(app, db_url=None):
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     #initialises sqlalchemy extension. We give it our flask app so it can connect it to sqlalchemy
     db.init_app(app)
+
+def configure_mail(app):
+    app.config['MAIL_SERVER'] = 'smtp.example.com'
+    app.config['MAIL_PORT'] = 587
+    app.config['MAIL_USE_TLS'] = True
+    app.config['MAIL_USERNAME'] = 'your-email@example.com'
+    app.config['MAIL_PASSWORD'] = 'your-password'
+
+    mail.init_app(app)  #Init flask-mail
 
 
 app = create_app()
