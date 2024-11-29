753f4cfa9668b9d590c4821bc73c910fef85f8fa
B. Drljević <87530056+Drxy0@users.noreply.github.com>
2024-11-29 22:59:02 +0100

    Edit friendship model

--- flask-engine/app/models/friendship.py
+++ flask-engine/app/models/friendship.py
@@ -4,5 +4,6 @@ from app.database import db
 friendship = db.Table(
     'friendships',
     db.Column('user_id', db.Integer, db.ForeignKey('users.id'), primary_key=True),
-    db.Column('friend_id', db.Integer, db.ForeignKey('users.id'), primary_key=True)
+    db.Column('friend_id', db.Integer, db.ForeignKey('users.id'), primary_key=True),
+    db.Column('acceepted', db.Boolean, default=False)
 )
